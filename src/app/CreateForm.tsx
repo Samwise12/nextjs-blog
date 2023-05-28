4a7d10aaad74f19513a95bbbc9120c3ca4a2d49e
samwise12 <sfelder1@binghamton.edu>
2023-05-28 13:59:38 -0400

    a

--- src/app/CreateForm.tsx
+++ src/app/CreateForm.tsx
@@ -9,7 +9,7 @@ export default function CreateForm() {
 
 	const router = useRouter();
 
-	const handleClick = async (e: string) => {	
+	const handleClick = async (e: React.FormEvent) => {	
 		e.preventDefault();
 		try {
 			const response = await fetch('http://localhost:8080/api/b', {
